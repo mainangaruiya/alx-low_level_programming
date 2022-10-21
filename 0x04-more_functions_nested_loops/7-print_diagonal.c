1579e053167c6ea16648e4ba217266013904fb69
mainangaruiya <003paulmaina@gmail.com>
2022-10-21 04:35:52 -0700

    diagoal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+void print_diagonal(int n)
+{
+	int co, sp:
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
+			for (sp = 1; sp < co; sp++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
