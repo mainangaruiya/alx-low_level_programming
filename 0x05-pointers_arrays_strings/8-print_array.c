2fb1af2b5d2d7b915f8eeb2f2ea8d0e4abee49f8
mainangaruiya <003paulmaina@gmail.com>
2022-10-25 03:55:05 -0700

    n elements of ineger

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		printf("%d", a[j]);
+
+		if (j != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	
+	printf("\n");
+}
