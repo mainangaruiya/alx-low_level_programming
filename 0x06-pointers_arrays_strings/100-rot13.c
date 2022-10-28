7b36f3f555cf0b72bf4f5f026b9ade41d31fecf2
mainangaruiya <003paulmaina@gmail.com>
2022-10-28 13:32:25 -0700

    rot 143

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+char *rot13(char *n)
+{
+	int x, rot_c = 13, i = 0;
+	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
+		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
+		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
+		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
+		'Z', 'm', 'z'};
+	while (n[i] != '\0')
+	{
+		for (x = 0; x <= 51; x++)
+		{
+			if (n[i] == toswap[x])
+			{
+				n[i] = n[i] + rot_c;
+				x = 51;
+			}
+			rot_c = rot_c * -1;
+		}
+		i++;
+	}
+	return (n);
+}
