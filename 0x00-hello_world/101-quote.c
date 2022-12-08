f027c8caa2ab92f80e4d3303793b476fce1b13b0
mainangaruiya <100405059+mainangaruiya@users.noreply.github.com>
2022-12-08 11:57:35 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 
-/**
- * * main - prints and that piece of art is useful - Dora Korpar, 2015-10-19,
- *                followed by a new line, to standard error.
- *
- * Return: 1 if successful.
- * Return: Always 1.
- */
 int main(void)
 {
-	write(2,
-	      and that piece of art is useful\
-	      - Dora Korpar, 2015-10-19\n,
-	      59);
-	return (1);
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (1);
 }
