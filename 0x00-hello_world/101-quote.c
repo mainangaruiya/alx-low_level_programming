a25a05681915f06df47aec3f75f726226707f49c
mainangaruiya <100405059+mainangaruiya@users.noreply.github.com>
2022-12-08 12:03:07 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	puts(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	return (1);
+return (1);
 }
