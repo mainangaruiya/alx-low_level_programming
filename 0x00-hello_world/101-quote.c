c13324cef50ac157d21c46ab21d3a39f872821d8
mainangaruiya <100405059+mainangaruiya@users.noreply.github.com>
2022-12-19 22:02:11 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
   printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-return (1);
+  return (1);
 }
