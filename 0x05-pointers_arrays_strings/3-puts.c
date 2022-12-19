e5483c0d2bec4bf29add0151ee35583b1916f9b7
mainangaruiya <100405059+mainangaruiya@users.noreply.github.com>
2022-12-19 12:10:30 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
-void _puts(char *str)
 
+/**
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
+ */
+void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
