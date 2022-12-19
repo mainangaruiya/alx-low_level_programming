214b0f2f873572d939c942625a510ab4a5023f67
mainangaruiya <100405059+mainangaruiya@users.noreply.github.com>
2022-12-19 22:06:49 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+  printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	return (1);
+return (1);
+  return (1);
 }
