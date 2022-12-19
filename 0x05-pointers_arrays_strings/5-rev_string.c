80a9a9bc197d3cedc37eefa778699cdc0f296c54
mainangaruiya <100405059+mainangaruiya@users.noreply.github.com>
2022-12-19 12:11:46 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * rev_string - a function that reverses a string
+ * @s: input to reverse
+ * Return: string and in reverse
+ */
 void rev_string(char *s)
 {
 	char rev = s[0];
