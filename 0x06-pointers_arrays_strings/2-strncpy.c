9fe9e03b7e4755dcbacac7d5227c2835cbddf498
mainangaruiya <100405059+mainangaruiya@users.noreply.github.com>
2022-12-21 10:46:09 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _strncpy - Entry point
+ * @dest: copy to
+ * @src: copy from
+ * @n: input number of char
+ * Return: Always 0 (Success)
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0, src_len = 0;
