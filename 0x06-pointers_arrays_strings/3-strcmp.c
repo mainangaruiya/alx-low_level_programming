e1424280a8517d1afdf156fdbd407fa581d3bafc
mainangaruiya <100405059+mainangaruiya@users.noreply.github.com>
2022-12-21 10:46:47 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: input one
+ * @s2: input two
+ * Return: Always 0 (Success)
+ */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
