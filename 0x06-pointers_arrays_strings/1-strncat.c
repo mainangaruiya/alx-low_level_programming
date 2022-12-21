b8e46f8409357dce765c6df73771bcc812b52e79
mainangaruiya <100405059+mainangaruiya@users.noreply.github.com>
2022-12-21 10:45:25 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: input of max bytes to be used
+ * Return: Always 0 (Success)
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
